5df7dbe62ad1fc037e40d8d1bfa4b9e4d8c717a5
Gautam yadav <114355514+yrahull595@users.noreply.github.com>
2022-10-07 14:25:39 +0530

    Update Binarysearch.cpp

--- Day_9/Binarysearch.cpp
+++ Day_9/Binarysearch.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+
+using namespace std;
+void binarysearch(int arr[] ,int low ,int high, int num)
+ {
+         if(low>high){
+         cout<<"Element not found";
+         }
+         else{
+        int mid = (low+high) /2;
+         if(arr[mid] == num) {
+             cout<<"element found at index :"<<mid;
+             exit(0);
+         }
+         else if(arr[mid]>num){
+             binarysearch(arr,low,mid-1,num);
+         }
+         else{
+          binarysearch(arr,mid+1,high,num);
+         }
+     }
+ }
+
+int main()
+{
+    int n,num,low = 0;
+    cout<<"enter the size of array :";
+    cin>>n;
+    if(n<1){
+        cout<<"Enter the valid size";
+    }else{
+    int arr[n];
+    int high = n;
+    cout<<"enter the "<<n<<" elements in sorted sequence :"<<endl;
+     for(int i = 0;i<n;i++){
+         cin>>arr[i];
+     }
+     cout<<"enter the desired elemen t to search for :";
+     cin>>num;
+       binarysearch( arr, low,high,num);
+ }
+}
 
